bb34875a0f23716fbb86d23365cd405a2ecc67f0
mvg.ljp <mvg.ljp@hotmail.com>
2023-05-17 23:03:31 -0300

    proyecto js 17/05

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 //entrada de datos > procesamiento de datos > salida
 //ETAPA 1 > ENTRADA DE DATOS
-let monto = parseInt(prompt("Ingrese Monto"));
-let plazo = parseInt(prompt("Ingrese cantidad de cutoas:(12/24/36):"));
-let tipoPlan = prompt("Ingrese el Tipo de Plan: (Exclusive/Premium/Basic)");
-const IVA = 21;
-let TNA;
-let tasa;
+let altura = parseInt(prompt("Ingrese Altura: (160/165/170/175/180/190/200):"));
+let busto = parseInt(prompt("Ingrese Medida de Busto:(80/85/90/95/100/105/110):"));
+let cintura = parseInt(prompt("Ingrese Medida de Cintura:(65/70/80/90/100):"));
+let cadera = parseInt(prompt("Ingrese Medida Cadera:(90/95/100/105/110):"));
+
+
+if (altura <= 165 && busto <= 85 && cintura <= 65 && cadera <= 95) {
+    talle = "S";
+  } else {
+    talle = "M";
+  }
+  
+  console.log("El talle es " + talle); // muestra por consola el talle correspondiente
+  
 
 //ETAPA 2 > procesamiento de datos
