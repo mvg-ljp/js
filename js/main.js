bde54e9c56084c79b7409810bdbeac2f79252545
mvg.ljp <mvg.ljp@hotmail.com>
2023-05-17 22:21:05 -0300

    proyecto js guerrero

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//entrada de datos > procesamiento de datos > salida
+//ETAPA 1 > ENTRADA DE DATOS
+let monto = parseInt(prompt("Ingrese Monto"));
+let plazo = parseInt(prompt("Ingrese cantidad de cutoas:(12/24/36):"));
+let tipoPlan = prompt("Ingrese el Tipo de Plan: (Exclusive/Premium/Basic)");
+const IVA = 21;
+let TNA;
+let tasa;
+
+//ETAPA 2 > procesamiento de datos
