827396fe9064e0f81c573b263698b511ed23d8b1
mvg.ljp <mvg.ljp@hotmail.com>
2023-05-22 01:49:47 -0300

    vadenuevo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 //entrada de datos > procesamiento de datos > salida
 //ETAPA 1 > ENTRADA DE DATOS
-let altura = parseInt(prompt("Ingrese Altura: (160/165/170/175/180/190/200):"));
-let busto = parseInt(prompt("Ingrese Medida de Busto:(80/85/90/95/100/105/110):"));
-let cintura = parseInt(prompt("Ingrese Medida de Cintura:(65/70/80/90/100):"));
-let cadera = parseInt(prompt("Ingrese Medida Cadera:(90/95/100/105/110):"));
-
-
-if (altura <= 165 && busto <= 85 && cintura <= 65 && cadera <= 95) {
-    talle = "S";
-  } else {
-    talle = "M";
-  }
-  
-  console.log("El talle es " + talle); // muestra por consola el talle correspondiente
+let plan = prompt( "Ingrese su Plan: (Basic/Premium/Exclusive):");
+let meses = parseInt(prompt("Ingrese Periodo de Suscripción:(6/12/24):"));
+const IVA = 21;
+let descuento;
   
 
 //ETAPA 2 > procesamiento de datos
